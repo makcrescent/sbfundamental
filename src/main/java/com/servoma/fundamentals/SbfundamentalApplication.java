559ee9e33cd00a353c4b869e62123b72bd128b9e
aslamkhan <makcrescent@gmail.com>
2021-10-14 11:22:38 +0530

    committing fundamental first time

---
+++ src/main/java/com/servoma/fundamentals/SbfundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.servoma.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbfundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbfundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servoma/fundamentals/SbfundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servoma.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbfundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
